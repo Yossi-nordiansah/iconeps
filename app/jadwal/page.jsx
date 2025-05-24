5d0f23bf9664edc334c7632615ec0c26f493ccfd
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2025-05-24 15:47:00 +0700

    jadwal page diperbaiki

--- app/jadwal/page.jsx
+++ app/jadwal/page.jsx
@@ -34,35 +34,63 @@ const Jadwal = () => {
           <img src="/images/calendar.png" alt="" className='w-10' />
           <h1 className='sm:text-xl text-lg font-robotoBold'>Periode Januari - Februari 2025</h1>
         </div>
-
-        <div className='flex lg:px-14 md:px-3 px-2 md:justify-between justify-center gap-4'>
-          <img src="/images/jadwal2.png" alt="" className='max-w-96 h-auto min-w-56 w-full object-contain md:block hidden' />
-          <div>
-            <h1 className='font-semibold text-center mb-2'>Kelas A (Weekend Online)</h1>
-
-            {/* table */}
-            <table className='table h-fit w-fit text-center lg:text-nowrap'>
-              <thead>
-                <tr className='bg-blue-600 text-white'>
-                  <th className='text-center sm:text-base text-[10px]'>Hari</th>
-                  <th className='text-center sm:text-base text-[10px]'>Tanggal</th>
-                  <th className='text-center sm:text-base text-[10px] sm:px-7'>Jam Mulai</th>
-                  <th className='text-center sm:text-base text-[10px] sm:px-7'>Jam Selesai</th>
-                  <th className='text-center sm:text-base text-[10px] sm:px-7'>Agenda</th>
-                </tr>
-              </thead>
-              <tbody>
-                {jadwalKelasA.map((item, index) => (
-                  <tr key={index}>
-                    <td className='text-center sm:text-base text-[10px]'>{item.hari}</td>
-                    <td className='text-center sm:text-base text-[10px]'>{item.tanggal}</td>
-                    <td className='text-center sm:text-base text-[10px]'>{item.jamMulai}</td>
-                    <td className='text-center sm:text-base text-[10px]'>{item.jamSelesai}</td>
-                    <td className='text-center sm:text-base text-[10px]'>{item.agenda}</td>
+        <div className='flex lg:flex-row flex-col lg:px-0 md:px-3 px-2 md:justify-between justify-center gap-4'>
+          <div className='w-fit lg:mx-0 mx-auto mb-4'>
+            <h1 className='text-2xl font-robotoBold text-center mb-2'>Jadwal Pusbas</h1>
+            <div>
+              <h1 className='font-semibold bg-yellow-400 px-2 py-1'>Kelas A (Weekend Online)</h1>
+              {/* table */}
+              <table className='table h-fit w-fit text-center lg:text-nowrap'>
+                <thead>
+                  <tr className='bg-blue-500 text-white'>
+                    <th className='text-center sm:text-base text-[10px]'>Hari</th>
+                    <th className='text-center sm:text-base text-[10px]'>Tanggal</th>
+                    <th className='text-center sm:text-base text-[10px] lg:px-7'>Jam Mulai</th>
+                    <th className='text-center sm:text-base text-[10px] lg:px-7'>Jam Selesai</th>
+                    <th className='text-center sm:text-base text-[10px] lg:px-7'>Agenda</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
+                </thead>
+                <tbody>
+                  {jadwalKelasA.map((item, index) => (
+                    <tr key={index}>
+                      <td className='text-center sm:text-base text-[10px]'>{item.hari}</td>
+                      <td className='text-center sm:text-base text-[10px]'>{item.tanggal}</td>
+                      <td className='text-center sm:text-base text-[10px]'>{item.jamMulai}</td>
+                      <td className='text-center sm:text-base text-[10px]'>{item.jamSelesai}</td>
+                      <td className='text-center sm:text-base text-[10px]'>{item.agenda}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div>
+            <h1 className='text-2xl font-robotoBold text-center mb-2'>Jadwal Puskom</h1>
+            <div className='w-fit lg:mx-0 mx-auto'>
+              <h1 className='font-semibold bg-[#00ff00] px-2 py-1'>Kelas A (Weekend Online)</h1>
+              {/* table */}
+              <table className='table h-fit w-fit text-center lg:text-nowrap'>
+                <thead>
+                  <tr className='bg-blue-500 text-white'>
+                    <th className='text-center sm:text-base text-[10px]'>Hari</th>
+                    <th className='text-center sm:text-base text-[10px] sm:px-7'>Jam Mulai</th>
+                    <th className='text-center sm:text-base text-[10px] sm:px-7'>Jam Selesai</th>
+                    <th className='text-center sm:text-base text-[10px] sm:px-7'>Agenda</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {jadwalKelasA.map((item, index) => (
+                    <tr key={index}>
+                      <td className='text-center sm:text-base text-[10px]'>{item.hari}</td>
+                      <td className='text-center sm:text-base text-[10px]'>{item.jamMulai}</td>
+                      <td className='text-center sm:text-base text-[10px]'>{item.jamSelesai}</td>
+                      <td className='text-center sm:text-base text-[10px]'>{item.agenda}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
